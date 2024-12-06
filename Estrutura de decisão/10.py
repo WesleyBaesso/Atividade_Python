9064ad1ab6318769d0fd1789180af7117247e3fe
WESLEY BAESSO <wesley.baesso@univille.br>
2024-12-06 20:20:11 -0300

    Atualização

--- Estrutura de decisão/10.py
+++
@@ -1,8 +0,0 @@
-a = float(input('info o valor 01: '))
-b = float(input('info o valor 02: '))
-c = float(input('info o valor 03: '))
-r = a
-
-if a == 0:
-    print('não é equação de segundo grau')
-
--- Matrizes/exercicio1.py
+++
@@ -1,9 +0,0 @@
-"""
-Faça um programa para ler e imprimir uma matriz 2 × 4 de números inteiros.
-"""
-matriz = [[0, 0, 0, 0], [0, 0, 0, 0]]
-
-for l in range(len(matriz)):
-    for c in range(len(matriz[l])):
-        matriz[l][c] = int(input('Digite numero: '))
-print(matriz)
