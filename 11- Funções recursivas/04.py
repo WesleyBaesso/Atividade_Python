0bcb98dc16bf00a84a6bd9c42e1a992c55f289db
WesleyBaesso <100739370+WesleyBaesso@users.noreply.github.com>
2024-12-06 21:31:34 -0300

    Update 04.py

--- 11- Funções recursivas/04.py
+++ 11- Funções recursivas/04.py
@@ -1,13 +1,13 @@
-def mdc(x, y):
-    if y == 0:
-        return x
+def fibonacci(N):
+    if N == 0:
+        return 0
+    elif N == 1:
+        return 1
     else:
-        return mdc(y, x % y)
-
+        return fibonacci(N - 1) + fibonacci(N - 2)
 def main():
-    x = int(input("Digite o primeiro número (x): "))
-    y = int(input("Digite o segundo número (y): "))
-    resultado = mdc(x, y)
-    print(f"O MDC de {x} e {y} é: {resultado}")
+    N = int(input("Digite um número inteiro positivo N para encontrar o N-ésimo termo da sequência Fibonacci: "))
+    resultado = fibonacci(N)
+    print(f"O {N}-ésimo termo da sequência Fibonacci é: {resultado}")
 if __name__ == "__main__":
     main()
